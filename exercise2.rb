dc501de3e29d59ced3bee4353c0f8c7fb4e90b03
marmo002 <marmo_02@hotmail.com>
2018-02-04 19:30:45 -0500

    done with exercise2

---
+++ exercise2.rb
@@ -0,0 +1,20 @@
+# calculate tip for a $55.00 meal
+puts "here are $#{55 * 0.05} tip"
+puts "---------------------------------"
+
+# adding string with integer using "5"
+a = "I'm "
+b = 30
+c = " years old"
+puts a + b.to_s + c
+puts "---------------------------------"
+
+# puts the addition of two number using string interpolation
+num_1 = 45628
+num_2 = 7839
+puts "The result is #{num_1 * num_2}"
+puts "---------------------------------"
+
+#figure out the result of (10 < 20 && 30 < 20) || !(10 == 11) = true
+
+puts ((10 < 20 && 30 < 20) || !(10 == 11))
